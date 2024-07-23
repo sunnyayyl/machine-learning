8fc4c1a7b9f3e47eb2d98db5e404f94ec072a18a
sunnyayyl <sunny.ayyl@gmail.com>
2024-07-23 23:18:56 +0100

    Fix logistic regression implementation

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,3 @@
-import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
@@ -7,10 +6,12 @@ import ml
 x_raw = jnp.array(
     [[0.5, 1.5], [1.0, 1.0], [1.5, 0.5], [3.0, 0.5], [2.0, 2.0], [1.0, 2.5]],
 )
-normalizer = ml.get_z_score_normalizer(x_raw)
-x_train = normalizer(x_raw)
 y_train = jnp.array([0, 0, 0, 1, 1, 1], dtype=jnp.float32)  # .reshape(-1, 1)
 
+x_train = x_raw
+"""normalizer = ml.get_z_score_normalizer(x_raw)
+x_train = normalizer(x_raw)
+"""
 w, b, history = ml.gradient_descend_training_loop(
     x_train,
     y_train,
@@ -29,11 +30,14 @@ plt.xlabel("Epoch")
 plt.show()
 prediction = ml.logistic_predict_all(x_train, w, b)
 plt.scatter(x_raw[:, 0], x_raw[:, 1], c=y_train)
-plt.plot(jax.vmap(lambda x: jax.numpy.pow(w, -1.0) * (-b))(jax.numpy.arange(0, 3)))
+x1 = jnp.arange(-5, 5)
+x2 = ((-b) - x1 * w[0]) / w[1]
+print(f"x1: {x1} x2: {x2}")
+plt.plot(x1, x2)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 for i in range(x_raw.shape[0]):
-    x = jnp.array(x_raw[i])
-    x = normalizer(x)
+    """x = jnp.array(x_raw[i])
+    x = normalizer(x)"""
     print(f"Predicted: {ml.logistic_predict(x, w, b)}, Target: {y_train[i]}")
