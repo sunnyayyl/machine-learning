61236ff50573f39ea589870b592f956a02f899fc
sunnyayyl <sunny.ayyl@gmail.com>
2024-08-26 20:31:36 +0100

    Changes to activation so it works with NN

--- ml/activation.py
+++ ml/activation.py
@@ -1,27 +1,25 @@
 import jax
 import jax.numpy as jnp
 from jax import jit
-from jaxtyping import jaxtyped
+from jaxtyping import jaxtyped, Float, Array
 from typeguard import typechecked
-
-from ml.definition import FloatScalar
 
 
 @jaxtyped(typechecker=typechecked)
 @jit
 def linear(
-    z: FloatScalar,
-) -> FloatScalar:
+    z: Float[Array, "..."],
+) -> Float[Array, "..."]:
     return z
 
 
 @jaxtyped(typechecker=typechecked)
 @jit
-def sigmoid(z: FloatScalar) -> FloatScalar:
+def sigmoid(z: Float[Array, "..."]) -> Float[Array, "..."]:
     return 1 / (1 + jnp.exp(-z))
 
 
 @jaxtyped(typechecker=typechecked)
 @jit
-def relu(z: FloatScalar) -> FloatScalar:
+def relu(z: Float[Array, "..."]) -> Float[Array, "..."]:
     return jax.lax.max(0.0, z)
