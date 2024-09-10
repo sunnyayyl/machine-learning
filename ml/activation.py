Add softmax and fix ReLu

ml/activation.py
@@ -5,6 +5,12 @@ from jaxtyping import jaxtyped, Float, Array
 from typeguard import typechecked
 
 
+@jit
+def softmax(z: Float[Array, "..."]) -> Float[Array, "..."]:
+    z = z - jnp.max(z, axis=-1, keepdims=True)
+    return jnp.exp(z) / jnp.sum(jnp.exp(z))
+
+
 @jaxtyped(typechecker=typechecked)
 @jit
 def linear(
@@ -16,10 +22,14 @@ def linear(
 @jaxtyped(typechecker=typechecked)
 @jit
 def sigmoid(z: Float[Array, "..."]) -> Float[Array, "..."]:
-    return 1 / (1 + jnp.exp(-z))
+    # return 1 / (1 + jnp.exp(-z))
+    return jax.lax.logistic(z)
 
 
-@jaxtyped(typechecker=typechecked)
+@jax.custom_jvp
 @jit
 def relu(z: Float[Array, "..."]) -> Float[Array, "..."]:
     return jax.lax.max(0.0, z)
+
+
+relu.defjvps(lambda g, ans, x: jax.lax.select(x > 0, g, jax.lax.full_like(g, 0.0)))

ml/cost.py
@@ -5,7 +5,7 @@ from jax import jit, vmap
 from jaxtyping import jaxtyped, Array, Float
 from typeguard import typechecked
 
-from ml.activation import sigmoid
+from ml.activation import sigmoid, softmax
 from ml.definition import FloatScalar
 from ml.definition.functions import PredictFunction
 from ml.predict import predict
@@ -37,15 +37,18 @@ def regression_mean_squared_error(
     return mean_squared_error(y_train, y_predict)
 
 
-@jaxtyped(typechecker=typechecked)
 @jit
-def logistic_cost(y_train: Array, y_predict: Array) -> FloatScalar:
+def logistic_cost(y_train: Array, y_predict: Array) -> Array:
     return jnp.mean(
         -y_train * jnp.log(y_predict) - (1 - y_train) * jnp.log(1 - y_predict)
     )
 
 
-@jaxtyped(typechecker=typechecked)
 @jit
-def mean_squared_error(y_train: Array, y_predict: Array) -> FloatScalar:
+def mean_squared_error(y_train: Array, y_predict: Array) -> Array:
     return jnp.mean(jnp.pow(y_train - y_predict, 2))
+
+
+@jit
+def softmax_cross_entropy(y_train: Array, y_predict: Array) -> Array:
+    return -jnp.log(jnp.take_along_axis(softmax(y_train), y_predict, -1))