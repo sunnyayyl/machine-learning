d4e6f5d8d9277b68f4e63ba1b05827de77a4d5ec
sunnyayyl <sunny.ayyl@gmail.com>
2024-09-10 23:02:00 +0100

    Improve MLP prediction on a polynomial and attempts at fixing MNIST prediction

--- examples/nn.py
+++ examples/nn.py
@@ -10,7 +10,7 @@ from ml.cost import mean_squared_error
 from ml.definition import FloatScalar
 from ml.nn import Dense, partition, combine
 from ml.normalizer import get_z_score_normalizer
-from ml.tools import generate_data
+from ml.tools import generate_data, sample
 
 
 @jax.tree_util.register_pytree_node_class
@@ -18,8 +18,9 @@ class Model:
     def __init__(self, key: PRNGKeyArray):
         key, key1, key2, key3, key4, key5 = random.split(key, 6)
         self.layers = [
-            Dense(1, 5, key=key3, activation=jax.nn.relu),
-            Dense(5, 1, key=key4),
+            Dense(1, 3, key=key3, activation=jax.nn.relu),
+            # Dense(25, 10, key=key1, activation=jax.nn.relu),
+            Dense(3, 1, key=key4),
         ]
 
     def __call__(self, x: Float[Array, "feature_size"]) -> Float[Array, "unit"]:
@@ -57,6 +58,7 @@ def loss(
 ) -> FloatScalar:
     combined_model = combine(params, static)
     prediction = jax.vmap(combined_model)(x)
+    # return jax.numpy.abs(y - prediction).mean()
     return mean_squared_error(y, prediction)
 
 
@@ -67,9 +69,17 @@ optimizer = optax.adam(0.001)
 def train_step(param, static, x, y, opt_state):
     value, grad = jax.value_and_grad(loss)(param, static, x, y)
     update, opt_states = optimizer.update(grad, opt_state)
-    param = optax.apply_updates(param, update)
+    grad = jax.tree.map(lambda x: jax.numpy.clip(x, -1.0, 1.0), grad)
+    # jax.debug.print("{}", grad)
+    """param = optax.apply_updates(param, update)"""
+    # jax.debug.print("Old {}", params.layers[0].w)
+    """if not jax.numpy.isnan(value).any():
+        print(jax.tree.flatten(grad))
+        exit()"""
+    param = jax.tree.map(lambda w, g: w - 0.00001 * g, param, grad)
+    # jax.debug.print("New {}", params.layers[0].w)
     # jax.debug.print("{}", update)
-    # grad = jax.tree.map(lambda x: jax.numpy.clip(x, -1.0, 1.0), grad)
+    #
     return (
         param,
         value,
@@ -80,33 +90,44 @@ def train_step(param, static, x, y, opt_state):
 def training_loop(param, static, x, y):
     history = []
     opt_state = optimizer.init(param)
-    for i in range(35000):
+    for i in range(3000000):
         param, v, opt_state = train_step(param, static, x, y, opt_state)
-        history.append(v)
+
+        if i % 1000 == 0:
+            history.append(v)
+            print(f"Epoch: {i} Loss: {v}")
     return param, static, history
 
 
 key, x, y = generate_data(
     key,
-    (1000,),
-    -1.0,
-    1.0,
-    lambda x: x,
+    (60,),
+    -100.0,
+    100.0,
+    lambda x: 3 * x + 4 * x**2 + 110,
+    20,
+    20,
 )
 
-inverse_normalizer, normalizer = get_z_score_normalizer(x)
+"""inverse_normalizer, normalizer = get_z_score_normalizer(x)
 y_inverse_noramlizer, y_normalizer = get_z_score_normalizer(y)
 x_norm = normalizer(x).reshape(-1, 1)
 y_norm = y_normalizer(y).reshape(-1, 1)
+"""
 x = x.reshape(-1, 1)
 y = y.reshape(-1, 1)
-print(x)
-print(f"Before: {loss(params, static, x_norm, y_norm)}")
-params, static, history = training_loop(params, static, x_norm, y_norm)
+print(f"Before: {loss(params, static, x, y)}")
+print(jax.tree.flatten(params)[0])
+plt.plot(x, (jax.vmap(model)(x)), c="yellow")
+
+params, static, history = training_loop(params, static, x, y)
 model = combine(params, static)
-print(f"After: {loss(params, static, x_norm, y_norm)}")
+print(f"After: {loss(params, static, x, y)}")
+print(jax.tree.flatten(params)[0])
+
 plt.scatter(x, y)
-plt.plot(x, y_inverse_noramlizer(jax.vmap(model)(x_norm)), c="red")
+plt.plot(x, (jax.vmap(model)(x)), c="red")
+
 plt.show()
 
 plt.plot(history)
